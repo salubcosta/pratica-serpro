9cbcd2a8082a7a37248efc1982c0b4f18e1f7279
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-07-31 21:52:08 -0300

    Algumas modificacoes em vetores e matrizes

--- java/pratica_001/Pratica_001/src/main/java/com/tipos/Tipos.java
+++ java/pratica_001/Pratica_001/src/main/java/com/tipos/Tipos.java
@@ -34,10 +34,33 @@ public class Tipos {
         }
         System.out.println(Arrays.toString(vetorDeInt));
 
+        int[] vetor3 = new int[4];
+        for (int i = 0; i < vetor3.length; i++) {
+            vetor3[i] = 100 * (i+1);
+        }
+        System.out.println(Arrays.toString(vetor3));
+
         // Matriz
+        System.out.println("Matriz:");
+
         double[][] matriz = {{0,1,2,3},{4,5,6,7}};
         System.out.println(Arrays.toString(matriz[0]));
         System.out.println(Arrays.toString(matriz[1]));
+
+        double[][] matriz2 = new double[2][3];
+
+        for (int i = 0; i < matriz2.length; i++) {
+            for (int j = 0; j < matriz2[i].length; j++) {
+                matriz2[i][j] = i * matriz2[i].length+j;
+            }
+        }
+
+        for (int i = 0; i < matriz2.length; i++) {
+            for (int j = 0; j < matriz2[i].length; j++) {
+                System.out.print(matriz2[i][j] + " ");
+            }
+            System.out.println();
+        }
 
 
     }
