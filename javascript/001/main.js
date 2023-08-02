cbda560b707d98997078beda4c49271a122467f0
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-08-02 17:23:04 -0300

    pegando elemento, variaveis e ternario

--- javascript/001/main.js
+++ javascript/001/main.js
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+//console.log('Hello, World!');
+var x;      // is undefined
+x = 5;      // is number
+x = "John"; // is string
+
+let nome = "Salumão";
+
+var elementoP = document.getElementById('elementoP');
+elementoP.innerHTML = nome; 
+
+const numero = 50;
+
+elementoP.innerHTML = numero > 50 ? "Maior" : "Menor";
