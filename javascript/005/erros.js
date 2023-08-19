80ce285772445fb7b702592d1a831bbc129b71fd
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-08-19 17:08:25 -0300

    visualizando erros no terminal

---
+++ javascript/005/erros.js
@@ -0,0 +1,8 @@
+//SyntaxError: Missing initializer in const declaration
+const numero = 0; // const precisa ser inicializado
+
+// SyntaxError: missing ) after argument list
+console.log(numero); // precisa fechar parênteses
+
+// ReferenceError: minhavar is not defined
+console.log(minhavar) // preciso declarar variável para utilizá-la
