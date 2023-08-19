b8a6562fa05cce3f0bcec1ef4b9fae3983d5f118
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-08-19 00:26:32 -0300

    arquivo para rever type boolean

---
+++ javascript/004/type-boolean.js
@@ -0,0 +1,9 @@
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1[0].toLowerCase()==texto2);
