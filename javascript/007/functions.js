f9e1deb884561c60af07b9f6a4b62e50a40f9b97
Salumao Barbosa da Costa <salubcosta@gmail.com>
2023-08-19 17:53:23 -0300

    brincando com functions

---
+++ javascript/007/functions.js
@@ -0,0 +1,11 @@
+function imprimeTexto(texto){
+    console.log(texto);
+}
+
+imprimeTexto("Hello, World!");
+
+
+// Uma forma pra criar uma função e já executá-la 
+(function(){
+    console.log('Hey!');
+})();
